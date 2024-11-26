982f5c1efc9bdd088210928c7c307f354a9c632e
Trần Tuấn Long <stu735105059@hnue.edu.vn>
2024-11-26 23:25:59 +0700

    Update database connection credentials in connectdb.php

--- connectdb.php
+++ connectdb.php
@@ -7,9 +7,9 @@
 
 // trên local
 $DB_HOST = 'nvme.h2cloud.vn';
-$DB_USER = 'root';
+$DB_USER = 'longitco_root';
 $DB_PASS = 'Long@2005';
-$DB_NAME = 'toan1'; //tên database
+$DB_NAME = 'longitco_toan1'; //tên database
 $conn=mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME) or die("Không thể kết nối tới cơ sở dữ liệu");
 if($conn){
     mysqli_query($conn,"SET NAMES 'utf8'");
