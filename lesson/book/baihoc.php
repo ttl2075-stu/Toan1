f029510a1562f96ba6d7ecd53148f575292e848d
Khổng Đỗ Thanh Huyền <stu735105046@hnue.edu.vn>
2024-12-05 21:56:42 +0700

    sửa 1 chỗ của d4k1

--- lesson/book/baihoc.php
+++ lesson/book/baihoc.php
@@ -22,7 +22,8 @@
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" integrity="sha256-+N4/V/SbAFiW1MPBCXnfnP9QSN3+Keu+NlB+0ev/YKQ=" crossorigin="anonymous" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-
+  <link rel="stylesheet" href="./assets/css_v2/style.css">
+  <link rel="stylesheet" href="./assets/css_v2/baihoc_php.css">
   <style>
    @import url('https://fonts.googleapis.com/css?family=Lato&display=swap');
 
